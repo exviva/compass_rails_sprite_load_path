e386062fcee8e87f1522cba03eacb65621be0760
Olek Janiszewski <olek.janiszewski@gmail.com>
2012-02-28 18:40:35 +0100

    Create an empty Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CompassRailsSpriteLoadPath::Application.config.secret_token = '41e3694c104ecb8f3f2e9f991190f3c79c73f1ceee3d3a1a9f90cd161a7afc8d3362136ef504dc9e472b83c2c8673b702ea4a0341fb148b36b8656ed7d04f43f'
